2e16baf662682959c80fa419383a95cd650936cf
phakjira <phakjira2000@gmail.com>
2018-12-16 12:15:51 +0700

    added new activity

--- NamJai/app/src/main/java/com/example/phakjira/namjai/Welcome.java
+++ NamJai/app/src/main/java/com/example/phakjira/namjai/Welcome.java
@@ -1,7 +1,9 @@
 package com.example.phakjira.namjai;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Welcome extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void newUser(View view){
+        Intent newUser = new Intent(this,FacebookLogin.class);
+        startActivity(newUser);
+    }
+
+    public void login(View view){
+        Intent login = new Intent(this,UserLogin.class);
+        startActivity(login);
     }
 }
