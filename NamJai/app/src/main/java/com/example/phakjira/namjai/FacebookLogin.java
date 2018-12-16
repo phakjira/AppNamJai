f1df27ae855888503dd4747f670bc9723a1f6a20
phakjira <phakjira2000@gmail.com>
2018-12-16 12:26:21 +0700

    added new activity

--- NamJai/app/src/main/java/com/example/phakjira/namjai/FacebookLogin.java
+++ NamJai/app/src/main/java/com/example/phakjira/namjai/FacebookLogin.java
@@ -1,13 +1,72 @@
 package com.example.phakjira.namjai;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
 
 public class FacebookLogin extends AppCompatActivity {
+    CallbackManager callbackManager;
+    AccessTokenTracker accessTokenTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facebook_login);
+
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+        LoginButton authButton = (LoginButton)this.findViewById(R.id.login_button);
+        authButton.setReadPermissions(Arrays.asList("user_email", "user_gender","user_name"));
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(
+                    AccessToken oldAccessToken,
+                    AccessToken currentAccessToken) {
+                // Set the access token using
+                // currentAccessToken when it's loaded or set.
+            }
+        };
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void onClicked(View view) {
+        Intent createUser = new Intent(this, CreateUser.class);
+        startActivity(createUser);
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
     }
 }
